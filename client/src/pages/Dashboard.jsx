5bb03544781ca4dc38e63fabb5e971edd895f585
Mohammed Parves <mdperrfan@gmail.com>
2026-04-01 22:45:13 +0600

    pop modal for create resume

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloudIcon, XIcon } from 'lucide-react'
 import { dummyResumeData } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 export default function Dashboard() {
+
   const [allresume, setAllresume] = React.useState([]);
   const [showCreateModal, setShowCreateModal] = React.useState(false);
-  const[showUploadModal, setShowUploadModal] = React.useState(false);
+  const [showUploadModal, setShowUploadModal] = React.useState(false);
+  const [title, setTitle] = React.useState('');
+  const [resumeFile, setResumeFile] = React.useState(null);
+  const [editResumeId, setEditResumeId] = React.useState(null);
 
   const user = { name: 'Parves' }
   const colors = ['#9333ea', '#d97706', '#dc2626', '#059669', '#2563eb', '#db2777', '#14b8a6', '#eab308', '#4f46e5', '#16a34a'];
-  
+  const navigate = useNavigate()
   const fetchAllResume = async () => {
     try {
 
@@ -20,6 +25,19 @@ export default function Dashboard() {
     }
   };
 
+  const createResume = async (e) => {
+    e.preventDefault();
+    try {
+      
+      // Implement API call to create resume here
+      setShowCreateModal(false);
+      navigate(`/app/builder?title=${'resumeId'}`);
+      // After successful creation, fetch the updated list of resumes
+      fetchAllResume();
+    } catch (error) {
+      console.error('Error creating resume:', error);
+    }
+  };
 
   React.useEffect(() => {
     fetchAllResume();
@@ -35,7 +53,7 @@ export default function Dashboard() {
 
         <div className='flex gap-4'>
           {/* Create Resume */}
-          <button className='flex flex-col items-center justify-center gap-2 w-full bg-white sm:max-w-36 h-48 rounded-lg text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 transition-all duration-300 cursor-pointer hover:shadow-lg'>
+          <button onClick={() => setShowCreateModal(true)} className='flex flex-col items-center justify-center gap-2 w-full bg-white sm:max-w-36 h-48 rounded-lg text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 transition-all duration-300 cursor-pointer hover:shadow-lg'>
             <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full' />
             <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>
               Create New Resume
@@ -57,32 +75,38 @@ export default function Dashboard() {
           {allresume.map((resume, index) => {
             const baseColor = colors[index % colors.length];
             return (
-             <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg cursor-pointer gap-2 border group hover:shadow-lg transition-all duration-300' style={{background:`linear-gradient(135deg,${baseColor}10,${baseColor}40)`, borderColor: baseColor+'40'}}>
+              <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg cursor-pointer gap-2 border group hover:shadow-lg transition-all duration-300' style={{ background: `linear-gradient(135deg,${baseColor}10,${baseColor}40)`, borderColor: baseColor + '40' }}>
 
-                <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{color:baseColor}} />
-                <p className='text-sm group-hover:scale-105 transition-all' style={{color:baseColor}}>
+                <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{ color: baseColor }} />
+                <p className='text-sm group-hover:scale-105 transition-all' style={{ color: baseColor }}>
                   {resume.title}
                 </p>
-                <p className='text-[11px] text-slate-400 absolute bottom-2  group-hover:text-slate-500 transition-all duration-300 text-center' style={{color:baseColor+'90'}}>
+                <p className='text-[11px] text-slate-400 absolute bottom-2  group-hover:text-slate-500 transition-all duration-300 text-center' style={{ color: baseColor + '90' }}>
                   Updated on {new Date(resume.updatedAt).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}
                 </p>
                 <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
                   <TrashIcon className='size-7 p-1.5 text-slate-700 hover:bg-white/50 transition-colors rounded' />
                   <PencilIcon className='size-7 p-1.5 text-slate-700 hover:bg-white/50 transition-colors rounded' />
                 </div>
-             </button>
+              </button>
             );
           })}
         </div>
+
+        {/* Create Resume Modal */}
         {
           showCreateModal && (
-
-            <form action="" className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
-              <div>
-                <h2>Create New Resume</h2>
-                <input type="text" placeholder="Resume Title" className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' />
-                <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700  transition-colors '>Create</button>
-                <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 transition-colors cursor-pointer' onClick={()=>setShowCreateModal(false)}/>
+            <form onSubmit={createResume} onClick={() => setShowCreateModal(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
+              <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+                <h2 className='text-xl font-bold mb-4'>Create New Resume</h2>
+                <input
+                  type="text"
+                  required
+                  placeholder="Resume Title"
+                  className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-green-600 focus:ring-2 focus:ring-green-600"
+                />
+                <button className='cursor-pointer w-full py-2 bg-green-600 text-white rounded hover:bg-green-700  transition-colors '>Create</button>
+                <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 transition-colors cursor-pointer' onClick={() => { setShowCreateModal(false); setTitle('') }} />
               </div>
             </form>
           )
