77e5dfed6dba5b5dc3b5959ad2a33531dae2f99e
Mohammed Parves <mdperrfan@gmail.com>
2026-04-03 01:18:55 +0600

    Completed the upload functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -15,6 +15,7 @@ export default function Dashboard() {
   const user = { name: 'Parves' }
   const colors = ['#9333ea', '#d97706', '#dc2626', '#059669', '#2563eb', '#db2777', '#14b8a6', '#eab308', '#4f46e5', '#16a34a'];
   const navigate = useNavigate()
+
   const fetchAllResume = async () => {
     try {
 
@@ -28,16 +29,29 @@ export default function Dashboard() {
   const createResume = async (e) => {
     e.preventDefault();
     try {
-      
       // Implement API call to create resume here
       setShowCreateModal(false);
-      navigate(`/app/builder?title=${'resumeId'}`);
+      navigate(`/app/builder/title=${'res123'}`);
       // After successful creation, fetch the updated list of resumes
       fetchAllResume();
     } catch (error) {
       console.error('Error creating resume:', error);
     }
   };
+
+
+  const uploadResume = async (e) => {
+    e.preventDefault();
+    try {
+      // Implement API call to upload resume here
+      setShowUploadModal(false);
+      navigate(`/app/builder/title=${'res123'}`);
+      fetchAllResume();
+    } catch (error) {
+      console.error('Error uploading resume:', error);
+    }
+  };
+
 
   React.useEffect(() => {
     fetchAllResume();
@@ -61,7 +75,7 @@ export default function Dashboard() {
           </button>
 
           {/* Upload Resume */}
-          <button className='flex flex-col items-center justify-center gap-2 w-full bg-white sm:max-w-36 h-48 rounded-lg text-slate-600 border border-dashed border-slate-300 group-hover:border-purple-500 transition-all duration-300 cursor-pointer hover:shadow-lg'>
+          <button onClick={() => setShowUploadModal(true)} className='flex flex-col items-center justify-center gap-2 w-full bg-white sm:max-w-36 h-48 rounded-lg text-slate-600 border border-dashed border-slate-300 group-hover:border-purple-500 transition-all duration-300 cursor-pointer hover:shadow-lg'>
             <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to-purple-500 text-white rounded-full' />
             <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>
               Upload Existing Resume
@@ -100,6 +114,8 @@ export default function Dashboard() {
               <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
                 <h2 className='text-xl font-bold mb-4'>Create New Resume</h2>
                 <input
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
                   type="text"
                   required
                   placeholder="Resume Title"
@@ -107,6 +123,42 @@ export default function Dashboard() {
                 />
                 <button className='cursor-pointer w-full py-2 bg-green-600 text-white rounded hover:bg-green-700  transition-colors '>Create</button>
                 <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 transition-colors cursor-pointer' onClick={() => { setShowCreateModal(false); setTitle('') }} />
+              </div>
+            </form>
+          )
+        }
+
+        {/* Upload Resume Modal */}
+        {
+          showUploadModal && (
+            <form onSubmit={uploadResume} onClick={() => setShowUploadModal(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
+              <div onClick={e => e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+                <h2 className='text-xl font-bold mb-4'>Upload Resume</h2>
+                <input
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                  type="text"
+                  required
+                  className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:border-green-600 focus:ring-2 focus:ring-green-600"
+                />
+                <div>
+                  <label htmlFor="resume-input" className='block text-sm text-slate-700'>
+                    Select Resume File
+                    <div className='flex flex-col items-center justify-center gap-2 border group text-slate-400 border-slate-400 border-dashed rounded-md p-4 py-10 my-4 hover:border-green-500 hover:text-green-700 cursor-pointer transition-colors'>
+                      {resumeFile ? (
+                        <p className='text-sm text-green-600'>{resumeFile.name}</p>
+                      ) : (
+                        <>
+                          <FilePenLineIcon className='size-7' />
+                          <p className='text-sm'>Click to select a file</p>
+                        </>
+                      )}
+                    </div>
+                  </label>
+                  <input id="resume-input" type="file" className='hidden' required accept='pdf' onChange={(e) => setResumeFile(e.target.files[0])} />
+                </div>
+                <button className='cursor-pointer w-full py-2 bg-green-600 text-white rounded hover:bg-green-700  transition-colors '>Upload</button>
+                <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 transition-colors cursor-pointer' onClick={() => { setShowUploadModal(false); setResumeFile(null) }} />
               </div>
             </form>
           )
