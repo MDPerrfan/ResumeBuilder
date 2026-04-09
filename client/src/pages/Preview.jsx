73f94fce3c8980d365213b300811f864f87b07e8
Mohammed Parves <mdperrfan@gmail.com>
2026-04-09 22:33:24 +0600

    Preview page done

--- client/src/pages/Preview.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Preview() {
-  return (
-    <div>
-      <h1>Preview Page</h1>
-    </div>
-  )
-}
---
+++ client/src/pages/Preview.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
+import Loading from '../Components/Loader'
+import ResumePreview from '../Components/ResumePreview'
+import { ArrowLeftIcon } from 'lucide-react'
+
+type Resume = {
+  _id: string,
+  template: string,
+  accent_color: string
+}
+
+export default function Preview() {
+  const { resumeId } = useParams<{ resumeId: string }>()
+
+  const [resumeData, setResumeData] = React.useState<Resume | null>(null)
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
+  const loadResume = async () => {
+
+    const found = dummyResumeData.find((resume: Resume) => resume._id === resumeId) || null
+    setResumeData(found)
+    setIsLoading(false)
+
+  }
+  useEffect(() => {
+    loadResume()
+    console.log(resumeData)
+  }, [])
+  return resumeData ? (
+    <div className='bg-slate-100'>
+      <div className='max-w-3xl mx-auto py-10'>
+        <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} classes={'py-4 bg-white'} />
+      </div>
+    </div>
+  ) : (
+    <div className='flex items-center justify-center min-h-screen bg-transparent'>
+      {isLoading? <Loading />:(
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <p className='text-center text-6xl text-slate-400 font-medium'>Resume not found</p>
+          <a href="/" className='mt-6 bg-purple-400 hover:bg-purple-700 text-white rounded-full px-6 h-9 ring-offset-1 ring-1 ring-purple-400 flex items-center transition-colors'>
+              <ArrowLeftIcon className='size-4 mr-2'/>
+              go to home 
+          </a>
+        </div>
+      )}
+    </div>
+  )
+}
