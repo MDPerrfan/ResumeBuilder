0251ae347e4a4eb9a015f25a160c67d805f27fbe
MDPerrfan <mdperrfan@gmail.com>
2026-04-23 18:59:54 +0600

    Database connected + Schema added

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI;
 
-  if (!mongoUri) {
-    throw new Error("MONGO_URI is not defined in environment variables");
+  try {
+    mongoose.connection.on("connected",()=>{console.log("Database Connected")})
+
+    let mongoUri = process.env.MONGO_URI;
+
+    const projectName = 'resume-builder'
+
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    if (mongoUri.endsWith('/')) {
+      mongoUri = mongoUri.slice(0, -1)
+    }
+
+    await mongoose.connect(`${mongoUri}/${projectName}`);
+  } catch (error) {
+    console.error("Failed to connect MongoDB", error)
   }
 
-  await mongoose.connect(mongoUri);
-  console.log("MongoDB connected");
 };
 
 module.exports = connectDB;
