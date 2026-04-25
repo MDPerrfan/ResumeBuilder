53474e9ae1cb14d7b0a0a59029b8f1f3fb440ad1
MDPerrfan <mdperrfan@gmail.com>
2026-04-25 22:40:30 +0600

    Updated import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { AuthenticateWithRedirectCallback, } from '@clerk/react'
+import { AuthenticateWithRedirectCallback, } from '@clerk/clerk-react'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
